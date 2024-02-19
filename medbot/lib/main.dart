a6fbd342c93d3e2cf9f733605127aacfb3ebe427
yclorpt <tdaniels058@gmail.com>
2024-02-18 19:34:59 -0700

    try this

--- medbot/lib/main.dart
+++ medbot/lib/main.dart
@@ -1,24 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:medbot/pages/home_page-this-one.dart';
+// import 'package:flutter/material.dart';
+// import 'package:medbot/pages/home_page-this-one.dart';
 
-void main() => runApp(new MyApp());
+// void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-      theme: ThemeData(
-          brightness: Brightness.light,
-          scaffoldBackgroundColor: const Color.fromARGB(255, 239, 239, 239),
-          primaryColor: const Color.fromARGB(255, 0, 0, 0)),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: HomePage(),
+//       theme: ThemeData(
+//           brightness: Brightness.light,
+//           scaffoldBackgroundColor: const Color.fromARGB(255, 239, 239, 239),
+//           primaryColor: const Color.fromARGB(255, 0, 0, 0)),
+//     );
+//   }
+// }
 
-/*
 import '../../medbot/lib/pages/screens.dart';
 import 'package:flutter/material.dart';
 
@@ -46,4 +45,4 @@ class MyApp extends StatelessWidget {
       },
     );
   }
-}*/
+}

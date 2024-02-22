2f7b2ad8fc653ae35fac5fa1d99e6bd347d4f6ae
cswansonbooth125 <cswansonbooth125@gmail.com>
2024-02-21 23:28:32 -0700

    added comments

--- medbot/lib/models/chat_message_model.dart
+++ medbot/lib/models/chat_message_model.dart
@@ -1,13 +1,16 @@
 import 'dart:convert';
 
+// Represents a chat message with a role and a list of chat parts
 class ChatMessageModel {
   final String role;
   final List<ChatPartModel> parts;
+
   ChatMessageModel({
     required this.role,
     required this.parts,
   });
 
+  // Converts the ChatMessageModel object to a map
   Map<String, dynamic> toMap() {
     return {
       'role': role,
@@ -15,6 +18,7 @@ class ChatMessageModel {
     };
   }
 
+  // Constructs a ChatMessageModel object from a map
   factory ChatMessageModel.fromMap(Map<String, dynamic> map) {
     return ChatMessageModel(
       role: map['role'] ?? '',
@@ -23,34 +27,42 @@ class ChatMessageModel {
     );
   }
 
+  // Converts the ChatMessageModel object to a JSON string
   String toJson() => json.encode(toMap());
 
+  // Constructs a ChatMessageModel object from a JSON string
   factory ChatMessageModel.fromJson(String source) =>
       ChatMessageModel.fromMap(json.decode(source));
 }
 
+// Represents a part of a chat message with text
 class ChatPartModel {
   final String text;
+
   ChatPartModel({
     required this.text,
   });
 
   List<ChatMessageModel> messages = [];
 
+  // Converts the ChatPartModel object to a map
   Map<String, dynamic> toMap() {
     return {
       'text': text,
     };
   }
 
+  // Constructs a ChatPartModel object from a map
   factory ChatPartModel.fromMap(Map<String, dynamic> map) {
     return ChatPartModel(
       text: map['text'] ?? '',
     );
   }
 
+  // Converts the ChatPartModel object to a JSON string
   String toJson() => json.encode(toMap());
 
+  // Constructs a ChatPartModel object from a JSON string
   factory ChatPartModel.fromJson(String source) =>
       ChatPartModel.fromMap(json.decode(source));
 }
--- medbot/lib/pages/chat.dart
+++ medbot/lib/pages/chat.dart
@@ -15,6 +15,7 @@ class Chat extends StatefulWidget {
 class _HomePageState extends State<Chat> {
   final ChatBloc chatBloc = ChatBloc();
   TextEditingController textEditingController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,144 +23,152 @@ class _HomePageState extends State<Chat> {
         automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
         actions: [
-          IconButton(onPressed: () {
-            showDialog(
-              context: context, 
-              builder: (context) => AlertDialog(
-                scrollable: true,
-                title: const Text('DISCLAIMER'),
-                content: const Text('MedBot is an artificial intelligence (AI) designed to offer general medical advice. It is currently in beta development and should not be considered a substitute for professional medical guidance. Please use the information provided with caution.\n\nMedBot is not a replacement for professional medical advice, diagnosis, or treatment. Consult with your physician or qualified health provider for personalized guidance.\n\nMedBot is in beta development. While efforts are made for accuracy, it may not reflect the most current medical standards. Updates are ongoing to improve reliability.\n\nExercise discretion when interpreting MedBot''s advice. Do not solely rely on it for critical healthcare decisions. In emergencies, seek immediate medical assistance.\n\nUsers acknowledge and assume risks associated with relying on AI-generated medical advice. Developers are not liable for any damages resulting from its use.\n\nProvide feedback on discrepancies or concerns. Your input helps enhance the accuracy of MedBot.\n\nBy using MedBot, you agree to this disclaimer. If you disagree, refrain from using the application.'),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.pop(context), 
-                    child: const Text('I Understand.')
+          IconButton(
+            onPressed: () {
+              showDialog(
+                context: context, 
+                builder: (context) => AlertDialog(
+                  scrollable: true,
+                  title: const Text('DISCLAIMER'),
+                  content: const Text('MedBot is an artificial intelligence (AI) designed to offer general medical advice. It is currently in beta development and should not be considered a substitute for professional medical guidance. Please use the information provided with caution.\n\nMedBot is not a replacement for professional medical advice, diagnosis, or treatment. Consult with your physician or qualified health provider for personalized guidance.\n\nMedBot is in beta development. While efforts are made for accuracy, it may not reflect the most current medical standards. Updates are ongoing to improve reliability.\n\nExercise discretion when interpreting MedBot''s advice. Do not solely rely on it for critical healthcare decisions. In emergencies, seek immediate medical assistance.\n\nUsers acknowledge and assume risks associated with relying on AI-generated medical advice. Developers are not liable for any damages resulting from its use.\n\nProvide feedback on discrepancies or concerns. Your input helps enhance the accuracy of MedBot.\n\nBy using MedBot, you agree to this disclaimer. If you disagree, refrain from using the application.'),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context), 
+                      child: const Text('I Understand.')
                     )
-                ],
-              )
+                  ],
+                )
               );
-          }, 
-          icon: const Icon(Icons.info)
+            }, 
+            icon: const Icon(Icons.info)
           ),
-        ]),
+        ]
+      ),
       bottomNavigationBar: const BottomNavBar(index: 1),
       extendBodyBehindAppBar: true,
-        body: BlocConsumer<ChatBloc, ChatState>(
-          bloc: chatBloc,
-          listener: (context, state) {},
-          builder: (context, state) {
-            switch (state.runtimeType) {
-              case ChatSuccessState:
-                List<ChatMessageModel> messages =
-                    (state as ChatSuccessState).messages;
-                return Container(
-                    child: Column(
+      body: BlocConsumer<ChatBloc, ChatState>(
+        bloc: chatBloc,
+        listener: (context, state) {},
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case ChatSuccessState:
+              List<ChatMessageModel> messages = (state as ChatSuccessState).messages;
+              return Container(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        height: 60,
-                        child: const Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("MedBot",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 24)),
-                          ],
-                        )),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      height: 60,
+                      child: const Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "MedBot",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold, 
+                              fontSize: 24
+                            )
+                          ),
+                        ],
+                      )
+                    ),
                     Expanded(
-                        child: ListView.builder(
-                      itemCount: messages.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                            margin: const EdgeInsets.only(
-                                bottom: 12, left: 16, right: 16),
+                      child: ListView.builder(
+                        itemCount: messages.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: const EdgeInsets.only(bottom: 12, left: 16, right: 16),
                             padding: const EdgeInsets.all(16),
                             decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                color: messages[index].role == "user"
-                                    ? Colors.blue
-                                    : const Color.fromARGB(255, 153, 153, 153)),
+                              borderRadius: BorderRadius.circular(16),
+                              color: messages[index].role == "user" ? Colors.blue : const Color.fromARGB(255, 153, 153, 153)
+                            ),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
-                                  messages[index].role == "user"
-                                      ? "You:"
-                                      : "MedBot:",
+                                  messages[index].role == "user" ? "You:" : "MedBot:",
                                   style: const TextStyle(
-                                      fontSize: 14,
-                                      color: Color.fromARGB(255, 0, 0, 0)),
+                                    fontSize: 14,
+                                    color: Color.fromARGB(255, 0, 0, 0)
+                                  ),
                                 ),
                                 const SizedBox(height: 8),
                                 Text(messages[index].parts.first.text),
                               ],
-                            ));
-                      },
-                    )),
+                            )
+                          );
+                        },
+                      )
+                    ),
                     if (chatBloc.generating)
                       Row(
                         children: [
                           Container(
-                              height: 100,
-                              width: 100,
-                              child: Lottie.asset('assets/loader.json')),
+                            height: 100,
+                            width: 100,
+                            child: Lottie.asset('assets/loader.json')
+                          ),
                           const SizedBox(width: 20),
                           const Text("Loading...")
                         ],
                       ),
                     Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 12, horizontal: 12),
-                      child: Row(children: [
-                        Expanded(
+                      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                      child: Row(
+                        children: [
+                          Expanded(
                             child: TextField(
-                          controller: textEditingController,
-                          style: const TextStyle(color: Colors.black),
-                          cursorColor: Theme.of(context).primaryColor,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(100),
-                              ),
-                              fillColor: Colors.white,
-                              hintText: "Ask medical questions here!",
-                              hintStyle: const TextStyle(
-                                  color: Color.fromARGB(255, 80, 80, 80)),
-                              filled: true,
-                              focusedBorder: OutlineInputBorder(
+                              controller: textEditingController,
+                              style: const TextStyle(color: Colors.black),
+                              cursorColor: Theme.of(context).primaryColor,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(100),
-                                  borderSide: BorderSide(
-                                      color: Theme.of(context).primaryColor))),
-                        )),
-                        const SizedBox(width: 12),
-                        InkWell(
-                          onTap: () {
-                            if (textEditingController.text.isNotEmpty) {
-                              String text = textEditingController.text;
-                              textEditingController.clear();
-                              chatBloc.add(ChatGenerateNewTextMessageEvent(
-                                  inputMessage: text));
-                            }
-                          },
-                          child: const CircleAvatar(
-                            radius: 34,
-                            backgroundColor: Colors.black,
-                            child: CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.white,
-                              child: Icon(Icons.send, color: Colors.grey),
+                                ),
+                                fillColor: Colors.white,
+                                hintText: "Ask medical questions here!",
+                                hintStyle: const TextStyle(color: Color.fromARGB(255, 80, 80, 80)),
+                                filled: true,
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                  borderSide: BorderSide(color: Theme.of(context).primaryColor)
+                                )
+                              ),
+                            )
+                          ),
+                          const SizedBox(width: 12),
+                          InkWell(
+                            onTap: () {
+                              if (textEditingController.text.isNotEmpty) {
+                                String text = textEditingController.text;
+                                textEditingController.clear();
+                                chatBloc.add(ChatGenerateNewTextMessageEvent(inputMessage: text));
+                              }
+                            },
+                            child: const CircleAvatar(
+                              radius: 34,
+                              backgroundColor: Colors.black,
+                              child: CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.white,
+                                child: Icon(Icons.send, color: Colors.grey),
+                              ),
                             ),
                           ),
-                        ),
-                      ]),
+                        ],
+                      ),
                     )
                   ],
-                ));
-              default:
-                return const SizedBox();
-            }
-          },
-        ));
+                )
+              );
+            default:
+              return const SizedBox();
+          }
+        },
+      )
+    );
   }
 }
-

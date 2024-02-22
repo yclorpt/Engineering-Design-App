1b74ead6b5a8c7eba6a88652920c71f14f96739e
yclorpt <tdaniels058@gmail.com>
2024-02-21 23:17:30 -0700

    Deleted unnecessary stuff

--- medbot/lib/pages/chat.dart
+++ medbot/lib/pages/chat.dart
@@ -163,25 +163,3 @@ class _HomePageState extends State<Chat> {
   }
 }
 
-// import 'package:medbot/widgets/bottom_nav_bar-this-one-i-think.dart';
-// import 'package:flutter/material.dart';
-
-// class Chat extends StatelessWidget {
-//   const Chat({super.key});
-//   static const routeName = '/chat';
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//           automaticallyImplyLeading: false,
-//           backgroundColor: Colors.transparent,
-//           actions: []),
-//       bottomNavigationBar: const BottomNavBar(index: 1),
-//       extendBodyBehindAppBar: true,
-//       body: ListView(
-//         padding: EdgeInsets.zero,
-//         children: [],
-//       ),
-//     );
-//   }
-// }
